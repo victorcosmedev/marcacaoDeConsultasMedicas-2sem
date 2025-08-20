8bfb04131e057d98dec14d5cc030167ffc06bc6d
victorcosmedev <victorcosme.profissional@gmail.com>
2025-08-20 19:31:01 -0300

    Adicionando botão na tela de perf

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -57,6 +57,13 @@ const ProfileScreen: React.FC = () => {
         />
 
         <Button
+          title="Editar Perfil"
+          onPress={() => navigation.navigate('EditProfile' as any)}
+          containerStyle={styles.button as ViewStyle}
+          buttonStyle={styles.editButton}
+        />
+
+        <Button
           title="Sair"
           onPress={signOut}
           containerStyle={styles.button as ViewStyle}
@@ -81,6 +88,10 @@ const styles = {
   },
   logoutButton: {
     backgroundColor: theme.colors.error,
+    paddingVertical: 12,
+  },
+  editButton: {
+    backgroundColor: theme.colors.success,
     paddingVertical: 12,
   },
 };
